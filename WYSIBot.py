ac641e394c995e87dc1d1bdc03218ca9e698d792
James Gray <jameslgray.home@gmail.com>
2023-08-17 10:44:05 +0100

    first commit

---
+++ WYSIBot.py
@@ -0,0 +1,51 @@
+import asyncio
+import json
+import os
+import sys
+
+import discord
+from discord.ext import commands, tasks
+
+# https://discord.com/api/oauth2/authorize?client_id=1013216514532454450&permissions=49469432790903&scope=applications.commands%20bot
+
+try:
+    with open(f"{os.path.dirname(__file__)}\\config.json", 'r') as json_file:
+        config = json.load(json_file)
+
+except IOError as e:
+    sys.exit(f"Could not load config: {e}")
+
+# Setting the intents
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(
+    command_prefix="!",
+    intents=intents,
+)
+
+
+@bot.event
+async def on_ready():
+    for file in os.listdir(f"{os.path.dirname(__file__)}\\cogs"):
+        if file.endswith(".py"):
+            extension = file[:-3]
+            try:
+                await bot.load_extension(f"cogs.{extension}")
+                print(f"Loaded cog: {extension}")
+
+            except Exception as e:
+                exception = f"{type(e).__name__}: {e}"
+                print(exception)
+
+    if config["sync_commands_globally"]:
+        await bot.tree.sync()
+        print(f'Synced commands')
+
+    print(f'Logged in as {bot.user}')
+
+
+
+if __name__ == '__main__':
+    bot.run(config['token'])
---
+++ cogs/WYSICommands.py
@@ -0,0 +1,117 @@
+import asyncio
+import random
+import threading
+import time
+
+import discord
+from discord.ext import commands
+from discord.ext.commands import Context
+
+
+class WYSICommands(commands.Cog, name="WYSICommands"):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.hybrid_command(
+        name="wysi",
+        description="when you see it",
+    )
+    async def wysi(self, context: Context):
+        return await context.send(f"WYSI")
+
+    @commands.hybrid_command(
+        name="karan",
+        description="nasa",
+    )
+    async def karan(self, context: Context):
+        return await context.send(f"nasa")
+
+    @commands.hybrid_command(
+        name="leah_kate",
+        description="<3",
+    )
+    async def leah_kate(self, context: Context):
+
+        options = [
+            "10 🌠 your selfish 🤬 ❗ 9 🎇 your jaded 😴 8 💋 The dumbest 🤪 guy I dated!! 🥱 7 🗣️ talk a big game 'til your naked.. 🍤 😳 Only 6 🗿 seconds 💁‍♀️ and I had to fake it 🫢 5 🤢 your toxic! ☢️ 4 🌹 cant 🚫 trust you 😵‍💫 🙅‍♀️ 3 🥉 you still got mommy 👩‍👧 issues ⁉️ 😂 2 🥈 years of your bullshit 💩 😒 i cant undo 1: 🥇 I hate the fact 😖 that you made me love you 💔 ⛓️ 🥀",
+            "So drink up 🍻⬆️ , get wasted 🥴️🤢 We only got one shot ☝🔫 , let's waste it 🗑️️ They'll never remember 🙅🧠 what your name is 🔤 No one gives a fuck 🤷🖕 if you're internet famous 🌐🥳️️ So stay up 🚫🛌 , get tattoos 🖋🦵️️ Make out with a stranger 💋🥰️️ in the bathroom 🚻 It's okay to scream it 😆🔊 , if you have to We're totally fucked 😵💀 Life sucks 🌱👎",
+            "https://i.ppy.sh/315b23f38b71792e29ea0b9cab1cc6ae6a1d4486/68747470733a2f2f692e696d6775722e636f6d2f7556496b7a75712e706e67",
+            "Twinkle 💫twinkle💫 little bitch👌🏼🐶 Just another narcissist👉😎👈 Hate your guts😡😡🫀you👊🏼 make me sick🤒 I'm so fucking over it🤬🤬🤬 Twinkle💫 twinkle💫 little bitch 👌🏼🐶 I wish 🙏you👊🏼 didn't exist🧑🏽❌️ Tried to turn⤵️ my life💔 to shit💩 I'm so fucking over it🤬🤬🤬",
+            ".-- .. -. -.- .-.. . 💫/ - .-- .. -. -.- .-.. . 💫 / .-.. .. - - .-.. . / -... .. - -.-. ....👌🏼🐶/ .--- ..- ... - / .- -. --- - .... . .-. / -. .- .-. -.-. .. ... ... .. ... - 👉😎👈/ .... .- - . / -.-- --- ..- .-. / --. ..- - ...😡😡🫀 -.-- --- ..- 👊🏼/ -- .- -.- . / -- . / ... .. -.-. -.-🤒/ .. .----. -- / ... --- / ..-. ..- -.-. -.- .. -. --. / --- ...- . .-. / .. - 🤬🤬🤬/ .-- .. -. -.- .-.. . 💫/ - .-- .. -. -.- .-.. . 💫 / .-.. .. - - .-.. . / -... .. - -.-. ....👌🏼🐶/ .. / .-- .. ... .... / 🙏 -.-- --- ..- 👊🏼/ -.. .. -.. -. .----. - / . -..- .. ... - 🧑🏽❌️/ .-. .. . -.. / - --- / - ..- .-. -. ⤵️/ -- -.-- / .-.. .. ..-. . 💔/ - --- / ... .... .. - 💩/ .. .----. -- / ... --- / ..-. ..- -.-. -.- .. -. --. / --- ...- . .-. / .. - 🤬🤬🤬"
+        ]
+
+        return await context.send(options[random.randint(0, len(options) - 1)])
+
+    @commands.hybrid_command(
+        name="roll",
+        description="Roll for 727",
+    )
+    async def roll(self, context: Context, upper_bound=1000):
+        if context.author.id == 187907815711571977:
+            return await context.send(f"{context.author.mention} rolled 727 with an upper bound of {upper_bound}!")
+
+        return await context.send(f"{context.author.mention} rolled {random.randint(0, upper_bound)} with an upper bound of {upper_bound}!")
+
+    @commands.hybrid_command(
+        name="send",
+        description="Secret",
+    )
+    async def message(self, context: Context, channel, message):
+        await context.guild.get_channel(channel).send(message)
+
+    @commands.hybrid_command(
+        name="transfer",
+        description="Transfer channel contents",
+    )
+    async def avatar(self, context: Context, destination_channel_id=1141487752169406665):
+
+
+        destination_channel = self.bot.get_channel(destination_channel_id)
+
+        with open("done.txt", "r+") as file:
+
+            message_ids = file.readlines()
+            seen = set(message_ids)
+
+            if message_ids:
+                newest_message_id = max(message_ids)
+                newest_message = await context.channel.fetch_message(int(newest_message_id))
+                history = context.channel.history(limit=50000, oldest_first=True, after=newest_message.created_at)
+                counter = len(message_ids)
+            else:
+                history = context.channel.history(limit=50000, oldest_first=True)
+                counter = 1
+
+            async for message in history:
+                try:
+                    print(counter)
+                    counter += 1
+
+                    if message.id not in seen:
+                        file.write(f"{message.id}\n")
+                    else:
+                        continue
+
+                    embed = discord.Embed()
+                    embed.set_author(name=message.author.name, icon_url=message.author.avatar.url)
+
+                    if message.attachments:
+                        await destination_channel.send(embed=embed)
+
+                        for attachment in message.attachments:
+                            await destination_channel.send(attachment.url)
+
+                    elif "gif" in message.content:
+                        await destination_channel.send(embed=embed)
+                        await destination_channel.send(message.content)
+
+                    else:
+                        embed.add_field(name="", value=message.content, inline=True)
+                        await destination_channel.send(embed=embed)
+
+                except Exception as e:
+                    print(e)
+
+
+async def setup(bot):
+    await bot.add_cog(WYSICommands(bot))
